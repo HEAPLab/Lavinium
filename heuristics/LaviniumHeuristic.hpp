4d089d2b45c9e4961f87f79c0ca08fd2c615c7b1
davidebaroffio-polimi <davide.baroffio@mail.polimi.it>
2025-01-21 17:17:50 +0100

    Added directory for lavinium heuristics definition

---
+++ heuristics/LaviniumHeuristic.hpp
@@ -0,0 +1,21 @@
+namespace Lavinium {
+//LAVINIUM-TODO
+class LaviniumHeuristic
+{
+private:
+  /* data */
+public:
+  LaviniumHeuristic(/* args */);
+  ~LaviniumHeuristic();
+};
+
+LaviniumHeuristic::LaviniumHeuristic(/* args */)
+{
+}
+
+LaviniumHeuristic::~LaviniumHeuristic()
+{
+}
+
+
+}
